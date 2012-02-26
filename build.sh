5e10295d786c3311328123500ff29015351bd40e
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-02-26 22:40:34 +0100

    once more

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [ ! -d "firebreath-1.6/build" ]; then
-  # need to download and extract FireBreath firstcd
+  # need to download and extract FireBreath first
+  mv firebreath-1.6 tmp
 	git clone git://github.com/firebreath/FireBreath.git -b firebreath-1.6 firebreath-1.6
+	mv tmp/* firebreath-1.6/
+	./prepmake.sh
 fi
 
 cd firebreath-1.6/build
