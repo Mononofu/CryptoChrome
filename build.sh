9bbfb256cc53d4b85386d3e5b22f727d78cb52e5
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-02-26 22:36:02 +0100

    fixed again

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "firebreath-1.6" ]; then
+if [ ! -d "firebreath-1.6/build" ]; then
   # need to download and extract FireBreath firstcd
 	git clone git://github.com/firebreath/FireBreath.git -b firebreath-1.6 firebreath-1.6
 fi
