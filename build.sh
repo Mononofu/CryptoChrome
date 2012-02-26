09ac5922c52e5b600fc4ee639a58a17b7b00eae5
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-02-26 22:47:47 +0100

    make sure build script cleans up tmp dir

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ if [ ! -d "firebreath-1.6/build" ]; then
   mv firebreath-1.6 tmp
 	git clone git://github.com/firebreath/FireBreath.git -b firebreath-1.6 firebreath-1.6
 	mv tmp/* firebreath-1.6/
+	rmdir tmp
 	cd firebreath-1.6
 	./prepmake.sh
 	cd ..
